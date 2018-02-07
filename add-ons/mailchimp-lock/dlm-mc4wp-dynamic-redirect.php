5665a98f01046719ab3e8a2da5428d63c0266e70
Barry Kooij <barrykooij@users.noreply.github.com>
2018-02-07 09:27:31 +0100

    Update dlm-mc4wp-dynamic-redirect.php
    Made compatible with Download Monitor 4.0

--- add-ons/mailchimp-lock/dlm-mc4wp-dynamic-redirect.php
+++ add-ons/mailchimp-lock/dlm-mc4wp-dynamic-redirect.php
@@ -1,11 +1,17 @@
 function mc4wp_dlm_dynamic_url( $url, $form ) {
 
 	if ( isset( $_POST['mc4wp_dlm_download_id'] ) && ! empty( $_POST['mc4wp_dlm_download_id'] ) ) {
-		$download = new DLM_Download( absint( $_POST['mc4wp_dlm_download_id'] ) );
+	
+		try {
+			/** @var DLM_Download $download */
+			$download = download_monitor()->service( 'download_repository' )->retrieve_single( absint( $_POST['mc4wp_dlm_download_id'] ) );
+			$url = $download->get_the_download_link();
+		} catch ( Exception $exception ) {
+			// no download with ID 4 found
+		}
 		
-		$url = $download->get_the_download_link();
 	}
-
+	
 	return $url;
 }
 
@@ -13,10 +19,11 @@ add_filter( 'mc4wp_form_redirect_url', 'mc4wp_dlm_dynamic_url', 20, 2 );
 
 function mc4wp_dlm_add_download_id( $content ) {
 	global $wp;
+	
 	if ( isset( $wp->query_vars ) && isset( $wp->query_vars['download-id'] ) ) {
 		$content .= "<input type='hidden' name='mc4wp_dlm_download_id' value='" . esc_attr( $wp->query_vars['download-id'] ) . "' />" . PHP_EOL;
 	}
-
+	
 	return $content;
 }
 
